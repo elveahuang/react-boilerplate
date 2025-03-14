06b38ce102b036a38afb39ec5cd0e16e69af1974
Elvea Huang <elvea.huang@gmail.com>
2025-03-14 10:55:47 +0800

    Initial commit

---
+++ apps/webapp/src/main.tsx
@@ -0,0 +1,11 @@
+import App from '@/App';
+import '@/theme/default.scss';
+import { setup } from '@/utils';
+import { log } from '@commons/core/utils';
+import '@commons/webapp/theme/default.scss';
+import { createRoot } from 'react-dom/client';
+
+setup().then((): void => {
+    createRoot(document.getElementById('app') as HTMLElement).render(<App />);
+    log(`App for webapp has been started.`);
+});
